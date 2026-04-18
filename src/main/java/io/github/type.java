ce9b52f7cd47e9968b3c3558bafcc17ffb7ebb09
MIAO_2026 <hei_1899@qq.com>
2026-04-18 11:12:53 +0800

    e没什么好说的

---
+++ src/main/java/io/github/type.java
@@ -0,0 +1,11 @@
+package io.github;
+/**
+ * 因为考虑扩展 此类型废弃
+ * 至于为什么不直接删掉 纯粹想用一下@Deprecated
+ * @deprecated 不要用这个了！！！替换为String!!!
+ * @since 1.0
+ * */
+@Deprecated(since = "1.0",forRemoval = true)
+public enum type {
+    INT,STRING,PLAYER,CODE
+}
